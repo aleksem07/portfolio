3f033815ae38c48153f6f659a944e4b53ae90ad2
aleksem07 <aleksem07@gmail.com>
2024-04-08 22:59:53 +0300

    feat: add scss

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  sassOptions: {
+    includePaths: [
+      'styles',
+      'node_modules',
+    ],
+  
+  }
+};
 
 export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "./normalize.scss";
+import "./globals.scss";
 
 const inter = Inter({ subsets: ["latin"] });
 
