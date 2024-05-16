36e11dc1131b79b013e407a3578469c40893c4eb
aleksem07 <aleksem07@gmail.com>
2024-05-17 00:02:42 +0300

    feat: add promo component

--- src/components/promo/Promo.tsx
+++ src/components/promo/Promo.tsx
@@ -1,20 +1,19 @@
-import reactLogo from "/src/assets/react.svg";
-import viteLogo from "/vite.svg";
+import styles from "/src/styles/components/promo.module.scss";
+import { Link } from "react-router-dom";
+import ROUTES from "../../common/routes";
+import Logo from "../logo";
 
 const Promo = () => {
   return (
-    <>
-      <div>promo</div>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+    <section className={styles.promo}>
       <h1 className="visually_hidden">Портфолио Алексей Семячкин</h1>
-    </>
+      <h2 className={styles.promo__title}>
+        Adaptive Logo Design for Your Brand
+      </h2>
+      <Link className={styles.promo__button} to={ROUTES.WORKS}>
+        Explore WORKS
+      </Link>
+    </section>
   );
 };
 
