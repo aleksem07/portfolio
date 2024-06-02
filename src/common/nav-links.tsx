f49f90bbb39c2bcbb85f25c73f15c64d0ac072fb
aleksem07 <aleksem07@gmail.com>
2024-06-02 20:15:23 +0300

    feat: add active link class

--- src/common/nav-links.tsx
+++ src/common/nav-links.tsx
@@ -8,22 +8,18 @@ const NavLinks = () => {
     {
       name: t("navigate.home"),
       href: `${ROUTES.MAIN}`,
-      active: true,
     },
     {
       name: t("navigate.about"),
       href: `${ROUTES.ABOUT}`,
-      active: false,
     },
     {
       name: t("navigate.works"),
       href: `${ROUTES.WORKS}`,
-      active: false,
     },
     {
       name: t("navigate.contacts"),
       href: `${ROUTES.CONTACTS}`,
-      active: false,
     },
   ];
 
