7083a7fc6a451a7ea66f625aed361916c7b211cd
aleksem07 <aleksem07@gmail.com>
2024-06-26 22:10:43 +0300

    feat: close burger resize window width

--- src/components/burger.tsx
+++ src/components/burger.tsx
@@ -1,26 +1,46 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "@/styles/components/burger.module.scss";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import { RootState } from "@/redux/store";
 import { setIsOpenNav } from "@/redux/nav-slice/nav-slice";
 
 const Burger = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
   const dispatch = useAppDispatch();
+
   const navIsOpenNow = useAppSelector(
     (state: RootState) => state.navIsOpen.value
   );
 
   const toggleMenu = () => {
-    const newIsOpen = !isOpen;
-    setIsOpen(newIsOpen);
+    const newIsOpen = !navIsOpenNow;
     dispatch(setIsOpenNav(newIsOpen));
   };
 
+  useEffect(() => {
+    const handleResize = () => {
+      dispatch(setIsOpenNav(false));
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (navIsOpenNow) {
+      document.body.classList.add("no-scroll");
+    } else {
+      document.body.classList.remove("no-scroll");
+    }
+  }, [navIsOpenNow]);
+
   return (
     <div
-      className={`${styles.menu_icon} ${isOpen ? styles.menu_icon_open : ""}`}
+      className={`${styles.menu_icon} ${
+        navIsOpenNow ? styles.menu_icon_open : ""
+      }`}
       onClick={toggleMenu}
     >
       <span></span>
